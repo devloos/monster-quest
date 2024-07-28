787a2af7fdf8f5beb99d5ef28ab09c273d2d7ce8
Carlos Aguilera <18aguilerac@gmail.com>
2024-07-28 01:09:31 -0700

    reorg

--- src/overlays/monster_index.py
+++ src/overlays/monster_index.py
@@ -43,7 +43,59 @@ class MonsterIndex:
 
         self.hovered_index = self.hovered_index % len(self.monsters)
 
-    def display_list(self, dt: float) -> None:
+    def draw_element_badge(self, rect: pg.FRect, monster: Monster) -> None:
+        # todo: add shadow effect and border
+        element = pg.Surface((60, 20), pg.SRCALPHA).convert_alpha()
+        element_rect = element.get_rect(
+            topleft=rect.bottomleft + vector(0, 10)
+        )
+
+        pg.draw.rect(
+            element, COLORS[monster.element],
+            pg.FRect(0, 0, 60, 20), border_radius=10
+        )
+
+        element_text = self.fonts['small'].render(
+            monster.element, False, COLORS['black']
+        )
+
+        self.screen.blit(
+            element_text,
+            element_text.get_frect(center=element_rect.center)
+        )
+
+        self.screen.blit(element, element_rect)
+
+    def draw_selected_badge(self, item_rect: pg.FRect) -> None:
+        center = item_rect.topright + vector(-13, 13)
+
+        pg.draw.circle(
+            self.screen, COLORS['gold'], center, radius=9, width=4
+        )
+
+    def draw_monster_name(
+        self, item_rect: pg.FRect, monster: Monster, text_color: str
+    ) -> tuple[pg.Surface, pg.FRect]:
+        monster_name = self.fonts['regular'].render(
+            monster.name, False, text_color
+        )
+
+        monster_name_rect = monster_name.get_frect(
+            topleft=item_rect.topleft + vector(20, 20)
+        )
+
+        self.screen.blit(monster_name, monster_name_rect)
+
+        return monster_name, monster_name_rect
+
+    def draw_monster_icon(self, item_rect: pg.FRect, monster: Monster) -> None:
+        icon_rect = monster.icon.get_rect(
+            center=item_rect.midright + vector(-55, 0)
+        )
+
+        self.screen.blit(monster.icon, icon_rect)
+
+    def draw_list(self, dt: float) -> None:
         start_index = 0
 
         if self.hovered_index >= self.visible_items:
@@ -68,53 +120,18 @@ class MonsterIndex:
 
             pg.draw.rect(self.screen, bg_color, item_rect)
 
-            monster_name = self.fonts['regular'].render(
-                monster.name, False, text_color
+            _, monster_name_rect = self.draw_monster_name(
+                item_rect, monster, text_color
             )
-            monster_name_rect = monster_name.get_frect(
-                topleft=item_rect.topleft + vector(20, 20)
-            )
-
-            # todo: add shadow effect and border
-            element = pg.Surface((60, 20), pg.SRCALPHA).convert_alpha()
-            element_rect = element.get_rect(
-                topleft=monster_name_rect.bottomleft + vector(0, 10)
-            )
-
-            pg.draw.rect(
-                element, COLORS[monster.element],
-                pg.FRect(0, 0, 60, 20), border_radius=10
-            )
-
-            element_text = self.fonts['small'].render(
-                monster.element, False, COLORS['black']
-            )
-
-            icon_rect = monster.icon.get_rect(
-                center=item_rect.midright + vector(-55, 0)
-            )
-
-            self.screen.blit(monster_name, monster_name_rect)
-
-            self.screen.blit(
-                element_text,
-                element_text.get_frect(center=element_rect.center)
-            )
-
-            self.screen.blit(element, element_rect)
-
-            self.screen.blit(monster.icon, icon_rect)
+            self.draw_monster_icon(item_rect, monster)
+            self.draw_element_badge(monster_name_rect, monster)
 
             if self.selected_index == index + start_index:
-                center = item_rect.topright + vector(-13, 13)
-
-                pg.draw.circle(
-                    self.screen, COLORS['gold'], center, radius=9, width=4
-                )
+                self.draw_selected_badge(item_rect)
 
     def update(self, dt: float) -> None:
         self._input()
 
         self.screen.blit(self.tint, (0, 0))
         pg.draw.rect(self.screen, 'black', self.main_rect)
-        self.display_list(dt)
+        self.draw_list(dt)
