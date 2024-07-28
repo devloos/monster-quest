308a9a0ea48ca9f8f80857ec87ec14ab84ec5cc5
Carlos Aguilera <18aguilerac@gmail.com>
2024-07-27 22:09:47 -0700

    icons

--- src/overlays/monster_index.py
+++ src/overlays/monster_index.py
@@ -28,19 +28,42 @@ class MonsterIndex:
                 self.main_rect.left, top, self.item_width, self.item_height
             )
 
-            pg.draw.rect(self.screen, 'red', self.item_rect)
+            pg.draw.rect(self.screen, 'dark gray', self.item_rect)
 
-            text = self.fonts['regular'].render(
+            monster_name = self.fonts['regular'].render(
                 monster.name, False, COLORS['white']
             )
-            text_rect = text.get_rect(
+            monster_name_rect = monster_name.get_frect(
                 topleft=self.item_rect.topleft + vector(20, 20)
             )
 
-            self.screen.blit(text, text_rect)
+            self.screen.blit(monster_name, monster_name_rect)
+
+            # todo: add shadow effect and border
+            element = pg.Surface((60, 20), pg.SRCALPHA).convert_alpha()
+            element_rect = element.get_rect(
+                topleft=monster_name_rect.bottomleft + vector(0, 10)
+            )
+
+            pg.draw.rect(
+                element, COLORS[monster.element],
+                pg.FRect(0, 0, 60, 20), border_radius=10
+            )
+
+            element_text = self.fonts['small'].render(
+                monster.element, False, COLORS['black']
+            )
+
+            element.blit(
+                element_text,
+                element_text.get_frect(center=element_rect.center)
+            )
+
+            self.screen.blit(element, element_rect)
 
             icon_rect = monster.icon.get_rect(
-                midright=self.item_rect.midright + vector(-25, 0))
+                midright=self.item_rect.midright + vector(-25, 0)
+            )
             self.screen.blit(monster.icon, icon_rect)
 
     def update(self, dt: float) -> None:
